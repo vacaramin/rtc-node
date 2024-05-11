9afb243bd6c6a1a7180882f5a02de76b34700734
Waqar Amin <vacaramin86@gmail.com>
2024-05-11 17:14:37 +0500

    chat

---
+++ controller/chat.controller.js
@@ -0,0 +1,12 @@
+const addMessage = (req, res) => {
+    res.send('Message Home Page');
+}
+const deleteMessage = (req, res) => {
+    res.send('Message Home Page');
+}
+const updateMessage = (req, res) => {
+    res.send('Message Home Page');
+}
+const getMessage = (req, res) => {
+    res.send('Message Home Page');
+}
